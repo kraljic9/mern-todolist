19516f6d21f8491449bb54c032976e0b3d197c7e
Pavle <kraljicpavle9@gmail.com>
2026-07-12 15:16:37 +0200

    Moved file inside of task

--- mern-todolist-app/app/api/task/[id]/route.js
+++ mern-todolist-app/app/api/task/[id]/route.js
@@ -18,6 +18,53 @@ export async function GET(request, {params}) {
 
         return NextResponse.json(task, {status: 200})
     }catch(err){
-        return NextResponse({message: 'Error accured:', err}, {status: 500})
+        return NextResponse.json({message: 'Error accured:', err}, {status: 500})
+    }
+}
+
+export async function PUT(request, {params}) {
+    try{
+
+        await connectDB();
+
+        const body = await request.json();
+        const {title, isCompleted} = body
+
+        const {id} = await params;
+        const task = await Task.findById(id);
+
+        if (!task) {
+             return NextResponse({message: `Error accured: could not find task with id ${id}`}, {status: 400})
+        }
+
+        if (title !== undefined) task.title = title
+        if (isCompleted !== undefined) task.isCompleted = isCompleted
+
+        const newTask = await task.save();
+
+        return NextResponse.json({ message: "Task edited successfully", newTask }, { status: 200 });
+
+    }catch(err) {
+        return NextResponse.json({message: 'Error accured:', err}, {status: 500})
+    }
+}
+
+export async function DELETE(request, {params}) {
+    try{
+
+        await connectDB();
+
+        const {id} = await params;
+
+        const deleteTask = await Task.findByIdAndDelete(id);
+
+        if (!deleteTask) {
+            return NextResponse.json(`Error could not find task with id ${id}`)
+        }
+
+        return NextResponse.json(`Task deleted sucessfully`, {status: 200});
+
+    }catch(err){
+        return NextResponse.json({message: 'Error accured:', err}, {status: 500})
     }
 }
