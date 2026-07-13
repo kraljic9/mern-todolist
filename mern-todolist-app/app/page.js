114fbf78bfdc2587b0d6ca2f6d010717833a3420
Pavle <kraljicpavle9@gmail.com>
2026-07-13 18:10:20 +0200

    Added function to add new todo and newTodo state

--- mern-todolist-app/app/page.js
+++ mern-todolist-app/app/page.js
@@ -9,7 +9,9 @@ export default function Home() {
     const [currentTask, setCurrentTask] = useState('');
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState('')
-    
+    const [newTask, setNewTask] = useState('')
+  
+    // Display todo to ui
     async function fetchTodos(){
       try{
         const response = await fetch('http://localhost:3000/api/task')
@@ -20,8 +22,6 @@ export default function Home() {
 
         const data = await response.json()
 
-        console.log(data)
-
         setTasks(data)
       }catch(err){
         console.log(err.message);
@@ -29,6 +29,7 @@ export default function Home() {
       }
     }
 
+    // Delete todo from UI
     async function removeTodo(id) {
   try{
       const response = await fetch(`http://localhost:3000/api/task/${id}`, {
@@ -47,24 +48,47 @@ export default function Home() {
       }
     } 
 
+    // Add todo to UI
+    async function addTodo(task) {
+      try{
+        const response = await fetch('http://localhost:3000/api/task', {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({title: task}) 
+        });
+
+         if(!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`)
+      }
+
+      const data = await response.json()
+
+      setTasks([...tasks, data])
+      }catch(err){
+         console.log(err.message);
+          setError(err.message)
+      }
+    }
+
     useEffect(() => {
       fetchTodos()
     },[])
-
 
   return (
     <div className="container">
     <div className="top">
     <h1>To do list app, add, edit, delete your tasks</h1>
-    <input placeholder="Write todo here"></input>
-    <button>Add todo</button>
+    <input placeholder="Write todo here" onChange={(e) => setNewTask(e.target.value)}></input>
+    <button className="add-btn" onClick={() => addTodo(newTask)}>Add todo</button>
     </div>
     
     <main>
       <div className="todos-container">
-        {tasks.map((task) => (
+        {tasks ? tasks.map((task) => (
            <div className="todo" key={task._id}><p className="todo-title">{task.title}</p><button className="edit-btn">EDIT</button><button className="delete-btn" onClick={() => removeTodo(task._id)}>DELETE</button></div>
-          ))}
+          )) : null}
       </div>
     </main>
     </div>
